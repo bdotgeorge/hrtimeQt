30693c715d5f7a099092de6233d43fc090b3af25
Vladimir Nagaev <nagaevvd1289@gmail.com>
2019-10-04 15:57:14 +0400

    3 methods & it'll be alive ......i hope so

--- dialog.cpp
+++ dialog.cpp
@@ -2,6 +2,10 @@
 
 #include "ui_dialog.h"
 
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QMap>
 #include <QMessageBox>
 #include <QPair>
 
@@ -14,6 +18,7 @@ Dialog::~Dialog()
 {
     delete ui;
 }
+
 // block of metods
 struct EveryPerson
 {
@@ -25,10 +30,14 @@ struct EveryPerson
 
 void toMap(EveryPerson &person)
 {
+    // QMap<int, int>::iterator it = person.howYouWorkInTisMonth.begin();
     for (int i = 0; i < person.dates.size(); i++) {
-        if (person.howYouWorkInTisMonth.find(person.dates[i] != person.howYouWorkInTisMonth.end())) {
+        if (person.howYouWorkInTisMonth.find(person.dates[i]) != person.howYouWorkInTisMonth.end()) {
+            int newValue = person.howYouWorkInTisMonth.value(person.howYouWorkInTisMonth.last()) + person.jobTime[i];
+            person.howYouWorkInTisMonth.value(person.howYouWorkInTisMonth.last(), newValue);
+        } else {
+            person.howYouWorkInTisMonth.insert(person.dates[i], person.jobTime[i]);
         }
-
         // person.howYouWorkInTisMonth.insert(person.dates[i],person.jobTime[i]);
     }
 }
@@ -72,13 +81,73 @@ EveryPerson testperson()
     return person;
 }
 
-QString toTableForOnePerson(EveryPerson person)
+QString toTextTableForOnePerson(EveryPerson person) // needed correct implimentation
 {
+    QMap<int, int>::iterator it = person.howYouWorkInTisMonth.begin();
     QString out = "";
+    QTextStream stream(&out);
     out.append(person.name);
     out.append("  | ");
+    int spaseForName = out.length();
+
+    for (auto &h : person.howYouWorkInTisMonth) {
+        stream << h;
+    }
+
+    out.append("\n");
+    int spaseForName2 = out.length() + person.name.length();
+
+    out.append("\n");
+    for (int i = 0; i < spaseForName2; i++) {
+        out.append("=");
+    }
+
+    out.append("\n");
 
     return out;
+}
+
+QString fromFile(const QString &pathFile)
+{
+    QFile inputFile(pathFile);
+    if (!inputFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "File not open: " + pathFile;
+    }
+
+    QTextStream stream(&inputFile);
+    QString out = stream.readAll();
+    return out;
+}
+
+bool toFile(QString outPutString, const QString &pathFile)
+{
+    QDir dir = QFileInfo(pathFile).absoluteDir();
+    if (!QDir(dir.absolutePath()).exists())
+        QDir().mkpath(dir.absolutePath());
+
+    QFile file(pathFile);
+    if (!file.open(QIODevice::WriteOnly)) {
+        qDebug() << "can't write to file" << pathFile;
+        return false;
+    }
+    QTextStream stream(&file);
+    stream << outPutString;
+
+    return true;
+}
+
+QList<EveryPerson> toStructData(QStringList)
+{
+    QList<EveryPerson> listForAllPersons;
+
+    // toDo inplimentatin
+    return listForAllPersons;
+}
+
+void converterWorker(QString &adress)
+{
+    QString inputtedText = QString(fromFile(adress));
+    QStringList stplittedByPersons = separatedByPerson(inputtedText);
 }
 
 // end of block
@@ -88,7 +157,7 @@ void Dialog::on_pushButton_clicked()
     QString adress = ui->lineAdressInput->text();
     QString test = "	Вход	01.08.2019	09:24	Выход	01.08.2019	17:05	7:41";
 
-    // ui->rsultlLabel->setNum(basicStringTokenaser(test).second);
+    ui->rsultlLabel->setText(toTextTableForOnePerson(testperson()));
 
     // QMessageBox::information(this, basicStringTokenaser(test).first, basicStringTokenaser(test).second);
 }
