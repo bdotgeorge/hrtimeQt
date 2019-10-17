c4377b109ee9ebb0a345f6114fd77e48f34e7b08
George B <bzzz2009@yandex.ru>
2019-10-17 16:53:24 +0400

    iu is fixed, tab is semi fixed, jpen file added,  checkbox is added

--- dialog.cpp
+++ dialog.cpp
@@ -8,6 +8,8 @@
 #include <QMap>
 #include <QMessageBox>
 #include <QPair>
+#include <QFileDialog>
+
 
 #include <iomanip>
 
@@ -22,8 +24,86 @@ Dialog::~Dialog()
 }
 
 // block of metods
+
+
+QString intToString(int a){
+    QString outString = "";
+    if (a >= 0 && a <= 9){
+        switch (a) {
+        case 0:
+            outString ="0";
+            break;
+        case 1:
+            outString ="1";
+            break;
+        case 2:
+            outString ="2";
+            break;
+        case 3:
+            outString ="3";
+            break;
+        case 4:
+            outString ="4";
+            break;
+        case 5:
+            outString ="5";
+            break;
+        case 6:
+            outString ="6";
+            break;
+        case 7:
+            outString ="7";
+            break;
+        case 8:
+            outString ="8";
+            break;
+        case 9:
+            outString ="9";
+            break;
+        }
+
+    }else if (a>=10) {
+        outString.append(intToString(a/10)).append(intToString(a % 10));
+
+}
+    return  outString;
+}
+
+
+QString toMonth (int month){
+    QString currentmonth = "";
+    switch (month) {
+    case 1: currentmonth = "январь";
+        break;
+    case 2: currentmonth = "февраль";
+        break;
+    case 3: currentmonth = "март";
+        break;
+    case 4: currentmonth = "апрель";
+        break;
+    case 5: currentmonth = "май";
+        break;
+    case 6: currentmonth = "июнь";
+        break;
+    case 7: currentmonth = "июль";
+        break;
+    case 8: currentmonth = "август";
+        break;
+    case 9: currentmonth = "сентбрь";
+        break;
+    case 10: currentmonth = "октябрь";
+        break;
+    case 11: currentmonth = "ноябрь";
+        break;
+    case 12: currentmonth = "декабрь";
+        break;
+    }
+    return currentmonth;
+}
+
 struct EveryPerson
 {
+    QString month;
     QString name;
     QVector<int> dates;
     QVector<int> jobTime;
@@ -54,18 +134,27 @@ int toMinutten(QString &s)
 QString toNormalHoursAndMinutes(int fullMinutes)
 {
     QString s;
-    s.append(fullMinutes / 60);
+    s.append(intToString(fullMinutes / 60));
     s.append(":");
-    s.append(fullMinutes % 60);
-    s.append(" ");
+    if (fullMinutes% 60 < 9){
+        s.append(intToString(0));
+         s.append(intToString(fullMinutes % 60));
+    }else {
+         s.append(intToString(fullMinutes % 60));
+}
+
+   // s.append(" ");
     return s;
 }
 
 QPair<QString, int> basicStringTokenaser(const QString &stt)
 {
     QPair<QString, int> today;
-    today.first = stt.split("\t", QString::SkipEmptyParts).at(1);
-    today.second = toMinutten(stt.split("\t", QString::SkipEmptyParts).last());
+    if (!stt.split("\t", QString::SkipEmptyParts).isEmpty()){
+        today.first = stt.split("\t", QString::SkipEmptyParts).at(1);
+        today.second = toMinutten(stt.split("\t", QString::SkipEmptyParts).last());
+    }
+
     return today;
 }
 
@@ -84,6 +173,7 @@ EveryPerson testperson()
     return person;
 }
 
+
 QString printStew(int e)
 {
     QString string = "";
@@ -91,10 +181,10 @@ QString printStew(int e)
         string.append("  ").append("-").append(" ");
     }
     if (e > 0 && e <= 9) {
-        string.append("  ").append(e).append(" ");
+        string.append("  ").append(intToString(e)).append(" ");
     }
     if (e > 9) {
-        string.append(" ").append(e).append(" ");
+        string.append(" ").append(intToString(e)).append(" ");
     }
     return string;
 }
@@ -106,7 +196,7 @@ int finder(QVector<int> &dates, int &date, int from)
         for (int i = 0; i < dates.size(); ++i) {
             if (dates[i] == date) {
                 result = i;
-            } else if (result == -2 && i == dates.size() - 1) {
+            } else if (i == dates.size() && dates[i] != date) {
                 result = -1;
             }
         }
@@ -126,7 +216,7 @@ int finder(QVector<int> &dates, int &date, int from)
 
 QString toTextTableForOnePerson(EveryPerson person) // needed correct implimentation
 {
-    QString out = "";
+    QString out  = "";
     // QTextStream stream(&out);
     /*
     stream << person.name;
@@ -145,26 +235,89 @@ QString toTextTableForOnePerson(EveryPerson person) // needed correct implimenta
     }
     */
     out.append(person.name);
-    out.append("  |");
-    for (int i = 0; i < 31; ++i) {
-        out.append(printStew(finder(person.dates, i, 0)));
+    out.append("|");
+    for (int i = 0; i <= 15; ++i) {
+        if (person.dates.contains(i)){
+            out.append(printStew(i));
+        }else {
+            out.append("    ");
+}
+        out.append("|");
+    }
+    int separatist = out.size();
+    out.append("\n");
+    for (int i = 0; i <= person.name.length(); ++i) {
+        out.append(" ");
+    }
+    for (int i = 0; i <= 15; ++i) {
+        if (person.dates.contains(i) && i < person.dates.size()) {
+            out.append(toNormalHoursAndMinutes(person.jobTime[i-1]));
+            out.append("|");
+        }else {
+            out.append("    |");
+}
+    }
+    out.append("\n");
+
+    for (int i = 0; i <= person.name.length(); ++i) {
+        out.append(" ");
+    }
+    for (int i = 16; i <= 31; ++i) {
+        if (person.dates.contains(i)){
+            out.append(printStew(i));
+        }else {
+            out.append("    ");
+}
         out.append("|");
     }
     out.append("\n");
-    for (int i = 0; i < person.name.length(); ++i) {
+    for (int i = 0; i <= person.name.length(); ++i) {
         out.append(" ");
     }
-    out.append(" ");
-    for (int i = 0; i < 31; ++i) {
-        if (finder(person.dates, i, 0) == i) {
+
+
+    //out.append(" ");
+    for (int i = 16; i <= 31; ++i) {
+        if (person.dates.contains(i) && i < person.dates.size()) {
             out.append(toNormalHoursAndMinutes(person.jobTime[i]));
             out.append("|");
-        }
+        }else {
+            out.append("    |");
+}
     }
     out.append("\n");
+    for (int i = 0; i <= separatist; ++i) {
+        out.append("=");
+    }
     out.append("\n");
     return out;
 }
+
+QString toStolbic(EveryPerson person){
+    QString out = "";
+
+    out.append(person.name);
+    out.append("\n");
+    for (int i = 0; i < person.dates.size(); ++i) {
+        out.append("\t");
+        out.append(person.month);
+        out.append("\n");
+        out.append(printStew(person.dates[i]));
+        out.append("\t");
+        if (person.dates.contains(i) && i < person.dates.size()) {
+            out.append(toNormalHoursAndMinutes(person.jobTime[i]));
+            out.append("\n");
+        }else {
+            out.append("\n");
+}
+    }
+
+
+
+
+    return  out;
+}
+
 
 QString fromFile(const QString &pathFile)
 {
@@ -201,21 +354,27 @@ EveryPerson toStruct(QString &onePersonStr)
     QStringList list = onePersonStr.split("\n", QString::SkipEmptyParts);
     if (!list.isEmpty()) {
         slave.name = list.first().split("\t", QString::SkipEmptyParts).first();
-        for (int i = 4; i < list.length(); ++i) {
+
+        for (int i = 3; i < list.length(); ++i) {
+            QPair<QString, int> pair;
             if (list[i].length() > 45) {
-                QPair<QString, int> pair = basicStringTokenaser(list[i]);
+                pair = basicStringTokenaser(list[i]);
                 int date = pair.first.split(".", QString::SkipEmptyParts).first().toInt();
-                if (finder(slave.dates, date, 0) == -1) {
+
+                if (!slave.dates.contains(date)) {
                     slave.dates.push_back(date);
                     slave.jobTime.push_back(pair.second);
-                } else if (finder(slave.dates, date, 0) != -1) {
-                    slave.jobTime[finder(slave.dates, date, 0)] += pair.second;
+                } else  {
+                    slave.jobTime[slave.dates.indexOf(date)] += pair.second;
                 }
             }
         }
+         //slave.month =toMonth( pair.first.split(".", QString::SkipEmptyParts).at(1).toInt());
     }
     return slave;
 }
+
+
 
 void personSummator(EveryPerson e) //  not nesessary now cause in Dialog::toStruct it already done
 {
@@ -241,26 +400,35 @@ QList<EveryPerson> toStructData(QStringList &listString)
     return listForAllPersons;
 }
 
-bool converterWorker(QString &adress)
+bool converterWorker(QString &adress, bool flag)
 {
-    QString outputAdres = adress.mid(0, adress.length() - 4).append("OUTPUT.txt");
+    QString outputAdres = adress.mid(0, adress.length() - 4).append(" OUTPUT.txt");
 
     QString inputtedText = QString(fromFile(adress));
     QStringList stplittedByPersons = separatedByPerson(inputtedText);
     QList<EveryPerson> parcedByPerson = toStructData(stplittedByPersons);
     QString readyText;
     for (EveryPerson e : parcedByPerson) {
-        readyText.append(toTextTableForOnePerson(e));
+        if (flag){
+        readyText.append(toTextTableForOnePerson(e));  //вывод в таблица
+        }
+        else {
+            readyText.append(toStolbic(e)); // вывод в столбик
+        }
     }
     return toFile(readyText, outputAdres);
 }
+
+
+
+
 
 // end of block
 
 void Dialog::on_pushButton_clicked()
 {
     QString adress = ui->lineAdressInput->text();
-    if (converterWorker(adress)) {
+    if (converterWorker(adress, true)) {
         ui->rsultlLabel->setText("все прошло хорошо ;-)");
         ui->lineAdressInput->clear();
 
@@ -272,3 +440,40 @@ void Dialog::on_pushButton_clicked()
     // ui->rsultlLabel->setText(toTextTableForOnePerson(testperson()));
     // QMessageBox::information(this, basicStringTokenaser(test).first, basicStringTokenaser(test).second);
 }
+
+// open file in dilog window )))))
+
+// george implimentation
+
+
+
+void Dialog::on_openButton_clicked()
+    {
+        QString openfile=QFileDialog::getOpenFileName(
+                    this,
+                    tr("Окрыть файл"),
+                    "C://",
+                    "Text File (*.txt)"
+                    );
+        //QMessageBox::information(this,tr("Имя файла"), openfile);
+
+        QString adress = openfile;
+
+
+
+
+        //доработать открытие файла
+        ui->lineAdressInput->setText(adress);
+        bool tablecheskbox = ui->checkBox->isDown();
+
+        if (converterWorker(adress, tablecheskbox)) {
+            ui->rsultlLabel->setText("таки кашерный файл");
+            ui->lineAdressInput->clear();
+
+        } else {
+            ui->rsultlLabel->setText(" ты подсовываешь  мне какую то дичь");
+        }
+
+
+    }
+
--- dialog.h
+++ dialog.h
@@ -23,6 +23,10 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+
+    void on_openButton_clicked();
+
+
 private:
     Ui::Dialog *ui;
 };
